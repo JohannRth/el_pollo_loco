95065329c4adbb9d6abdf62536afef2741a60249
JohannRth <johann.roth.e46@googlemail.com>
2024-11-22 15:54:52 +0100

    added pepe and chickens and clouds

--- js/game.js
+++ js/game.js
@@ -1,12 +1,10 @@
 let canvas;
-let ctx;
-let character = new Image();
+let world;
 
-function init(){
+function init() {
     canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
+    world = new World(canvas);
 
-    character.src = '../img/2_character_pepe/1_idle/idle/I-1.png'
+    console.log('My Character is', world.character);
 
-    ctx.drawImage(character, 20, 20, 50, 150);
 }
