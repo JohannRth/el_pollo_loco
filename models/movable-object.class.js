4c475858cf4acd3b770dd6c6197aaeb2f5c6d75d
JohannRth <johann.roth.e46@googlemail.com>
2024-11-27 15:08:09 +0100

    added cages to see there the pics colide

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -24,11 +24,22 @@ class MovableObject {
         return this.y < 180;
     }
 
-
     // loadImage('img/test.png');
     loadImage(path) {
         this.img = new Image(); // this.img = document.getElementById('image') <img id="image" src>
         this.img.src = path;
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    drawFrame(ctx) {
+        ctx.beginPath();
+        ctx.lineWidth = '5';
+        ctx.storkeStyle = 'red';
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.stroke();
     }
 
     /**
