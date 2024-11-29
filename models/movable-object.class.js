2b877cd66a184ddbd8e2203968afbfd1e078ac11
JohannRth <johann.roth.e46@googlemail.com>
2024-11-29 15:11:58 +0100

    added a status bar

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,11 +1,4 @@
-class MovableObject {
-    x = 120;
-    y = 280;
-    img;
-    height = 150;
-    width = 100;
-    imageCache = {};
-    currentImage = 0;
+class MovableObject extends DrawableObject {
     speed = 0.15;
     otherDirection = false;
     speedY = 0;
@@ -26,28 +19,12 @@ class MovableObject {
         return this.y < 180;
     }
 
-    // loadImage('img/test.png');
-    loadImage(path) {
-        this.img = new Image(); // this.img = document.getElementById('image') <img id="image" src>
-        this.img.src = path;
-    }
 
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
 
-    drawFrame(ctx) {
-        if (this instanceof Character || this instanceof Chicken || this instanceof Endboss) {
-            ctx.beginPath();
-            ctx.lineWidth = '5';
-            ctx.storkeStyle = 'red';
-            ctx.rect(this.x, this.y, this.width, this.height);
-            ctx.stroke();
-        };
-    }
+
 
     isColliding(mo) {
-        return this.x + this.width > mo.x && 
+        return this.x + this.width > mo.x &&
             this.y + this.height > mo.y &&
             this.x < mo.x &&
             this.y < mo.y + mo.height;
@@ -62,7 +39,7 @@ class MovableObject {
         }
     }
 
-    isHurt(){
+    isHurt() {
         let timepassed = new Date().getTime() - this.lastHit; // Differce in ms
         timepassed = timepassed / 1000; //Difference in s
         return timepassed < 1;
@@ -72,17 +49,6 @@ class MovableObject {
         return this.energy == 0;
     }
 
-    /**
-     * 
-     * @param {Array} arr  - ['img/image1.png', 'img/image2.png', ...]
-     */
-    loadImages(arr) {
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
 
     playAnimation(images) {
         let i = this.currentImage % images.length; // let i = 0 % 10;
