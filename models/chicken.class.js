15b8c5db95cb73b2f142898a508e2861f49c9247
JohannRth <johann.roth.e46@googlemail.com>
2025-01-10 14:39:47 +0100

    change the size of chickens

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,6 +1,7 @@
 class Chicken extends MovableObject {
-    y = 340;
-    height = 100;
+    y = 380;
+    height = 60;
+    width = 60;
     IMAGES_WALKING = [
         'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
