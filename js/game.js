34ab2206d148f929a8af765df78af2bfc7f15411
JohannRth <johann.roth.e46@googlemail.com>
2025-02-18 20:01:22 +0100

    added the functions of the buttons of the home menue to game.js

--- js/game.js
+++ js/game.js
@@ -8,3 +8,26 @@ function init() {
     console.log('My Character is', world.character);
     addTouchListeners(); // Event-Listener für die Buttons hinzufügen
 }
+
+function showMenu() {
+    document.getElementById('menu').style.display = 'block';
+    document.getElementById('game').style.display = 'none';
+    document.getElementById('instructions').style.display = 'none';
+    document.getElementById('imprint').style.display = 'none';
+}
+
+function startGame() {
+    document.getElementById('menu').style.display = 'none';
+    document.getElementById('game').style.display = 'block';
+    init();
+}
+
+function showInstructions() {
+    document.getElementById('menu').style.display = 'none';
+    document.getElementById('instructions').style.display = 'block';
+}
+
+function showImprint() {
+    document.getElementById('menu').style.display = 'none';
+    document.getElementById('imprint').style.display = 'block';
+}
